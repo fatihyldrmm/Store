6d6baedb666f09609d4a2f62492cf9120bf790cf
fatih yıldırım <105606171+fatihyldrmm@users.noreply.github.com>
2023-07-05 19:35:11 +0300

    first commit

---
+++ StoreApp/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Contracts;
+
+namespace StoreApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private IRepositoryManager _manager;
+
+        public CategoryController(IRepositoryManager manager)
+        {
+            _manager = manager;
+        }
+
+        public IEnumerable<Category> Index()
+        {
+            return _manager.Category.FindAll(false);
+        }
+    }
+}
