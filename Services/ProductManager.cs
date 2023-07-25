480520a7201f19505537e1b5d248b0febde97b37
fatih yıldırım <105606171+fatihyldrmm@users.noreply.github.com>
2023-07-25 11:32:08 +0300

    comit

--- Services/ProductManager.cs
+++ Services/ProductManager.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Entities.Dtos;
 using Entities.Models;
 using Repositories.Contracts;
 using Services.Contracts;
@@ -8,17 +10,20 @@ namespace Services
     {
         private readonly IRepositoryManager _manager;
 
-        public ProductManager(IRepositoryManager manager)
+        private readonly IMapper _mapper;
+        public ProductManager(IRepositoryManager manager, IMapper mapper)
         {
             _manager = manager;
+            _mapper = mapper;
         }
 
-        public void CreateProduct(Product product)
+        public void CreateProduct(ProductDtoForInsertion productDto)
         {
+            Product product = _mapper.Map<Product>(productDto);
             _manager.Product.Create(product);
             _manager.Save();
         }
-
+        
         public void DeleteOneProduct(int id)
         {
             var product = GetOneProduct(id, false);
@@ -42,12 +47,22 @@ namespace Services
             return product;
         }
 
-        public void UpdateOneProduct(Product product)
+        public ProductDtoForUpdate GetOneProductForUpdate(int id, bool trackChanges)
         {
-            var entity = _manager.Product.GetOneProduct(product.ProductId, true);
-            entity.ProductName = product.ProductName;
-            entity.Price = product.Price;
-            _manager.Save();
+            var product = GetOneProduct(id, trackChanges);
+            var productDto = _mapper.Map<ProductDtoForUpdate>(product);
+            return productDto;
+        }
+
+        public void UpdateOneProduct(ProductDtoForUpdate productDto)
+        {
+            // var entity = _manager.Product.GetOneProduct(productDto.ProductId, true);
+            // entity.ProductName = productDto.ProductName;
+            // entity.Price = productDto.Price;
+            // entity.CategoryId = productDto.CategoryId;
+            var entity = _mapper.Map<Product>(productDto);
+            _manager.Product.UpdateOneProduct(entity);
+             _manager.Save();
         }
     }
 }
